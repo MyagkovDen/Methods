4aff5f46e2ed2269eaf46a5b9357cfce421aabc8
Denis Myagkov <tomich-84@mail.ru>
2022-11-29 23:39:49 +0300

    add PrintArray

---
+++ PrintArray/Program.cs
@@ -0,0 +1,10 @@
+﻿void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
