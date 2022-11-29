0375e877e65660ac5d3ffd43fb9f7e457e276bf5
Denis Myagkov <tomich-84@mail.ru>
2022-11-29 23:41:29 +0300

    add FillArray

---
+++ FillArray/Program.cs
@@ -0,0 +1,10 @@
+﻿void FillArray(int[] collection) 
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
