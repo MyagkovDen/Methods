5fea2c822a5c9e4876c67f6cf83d70b91827714b
Denis Myagkov <tomich-84@mail.ru>
2022-11-29 23:37:06 +0300

    add GetNumberFromUser

---
+++ GetNumberFromUser/Program.cs
@@ -0,0 +1,13 @@
+﻿int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+
+        else
+            Console.WriteLine(errorMessage);
+    }
+}
